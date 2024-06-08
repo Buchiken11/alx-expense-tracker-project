c648853916f3c3dc3fa7f36983fb798bb68863c8
Buchiken11 <buhiken025@gmail.com>
2024-06-08 15:38:38 +0100

    oauth.py

--- oauth.py
+++ oauth.py
@@ -1,22 +1,72 @@
-from flask_oauthlib.client import OAuth
+from authlib.integrations.flask_client import OAuth
+from flask import current_app
 
 oauth = OAuth()
 
-google = oauth.remote_app(
-    'google',
-    consumer_key='your-google-client-id',
-    consumer_secret='your-google-client-secret',
-    request_token_params={
-        'scope': 'email',
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accoufewnts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-)
-
-# Add other providers similarly
-
 def init_oauth(app):
     oauth.init_app(app)
+
+    # Register Google OAuth client
+    oauth.register(
+        name='google',
+        client_id=current_app.config['GOOGLE_CLIENT_ID'],
+        client_secret=current_app.config['GOOGLE_CLIENT_SECRET'],
+        authorize_url='https://accounts.google.com/o/oauth2/auth',
+        authorize_params=None,
+        access_token_url='https://accounts.google.com/o/oauth2/token',
+        access_token_params=None,
+        refresh_token_url=None,
+        redirect_uri=None,
+        client_kwargs={
+            'scope': 'openid profile email',
+        }
+    )
+
+    # Register Facebook OAuth client
+    oauth.register(
+        name='facebook',
+        client_id=current_app.config['FACEBOOK_CLIENT_ID'],
+        client_secret=current_app.config['FACEBOOK_CLIENT_SECRET'],
+        authorize_url='https://www.facebook.com/dialog/oauth',
+        authorize_params=None,
+        access_token_url='https://graph.facebook.com/oauth/access_token',
+        access_token_params=None,
+        refresh_token_url=None,
+        redirect_uri=None,
+        client_kwargs={
+            'scope': 'email',
+        }
+    )
+
+    # Register Twitter OAuth client
+    oauth.register(
+        name='twitter',
+        client_id=current_app.config['TWITTER_CLIENT_ID'],
+        client_secret=current_app.config['TWITTER_CLIENT_SECRET'],
+        authorize_url='https://api.twitter.com/oauth/authenticate',
+        authorize_params=None,
+        access_token_url='https://api.twitter.com/oauth/access_token',
+        access_token_params=None,
+        refresh_token_url=None,
+        redirect_uri=None,
+        client_kwargs={
+            'scope': 'email',
+        }
+    )
+
+    # Register LinkedIn OAuth client
+    oauth.register(
+        name='linkedin',
+        client_id=current_app.config['LINKEDIN_CLIENT_ID'],
+        client_secret=current_app.config['LINKEDIN_CLIENT_SECRET'],
+        authorize_url='https://www.linkedin.com/oauth/v2/authorization',
+        authorize_params=None,
+        access_token_url='https://www.linkedin.com/oauth/v2/accessToken',
+        access_token_params=None,
+        refresh_token_url=None,
+        redirect_uri=None,
+        client_kwargs={
+            'scope': 'r_liteprofile r_emailaddress',
+        }
+    )
+
